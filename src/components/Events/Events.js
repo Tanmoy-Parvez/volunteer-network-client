edb38cb8a5bad64f33f531f3b54b32a85e564649
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-28 13:58:34 +0600

    query by email done

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import useAuth from '../../hooks/useAuth';
 
 const Events = () => {
+    const [events, setEvents] = useState([]);
+    const { user } = useAuth()
+    const email = user?.email;
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/registerEvents/${email}`)
+            .then(res => res.json())
+            .then(data => {
+                setEvents(data);
+            })
+    }, [])
     return (
         <div className="container mt-5">
-            <h1 className="marginTop">This is event page</h1>
+            <h1 className="marginTop">Your registered events: {events.length}</h1>
+            {
+                <Row xs={1} md={2} className="g-5 mt-5">
+                    {
+                        events.map(event => <Col key={event._id}>
+                            <Card>
+                                <Card.Img variant="top" src="" />
+                                <Card.Body className="rounded-3">
+                                    <Card.Title>{event?.activity}</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Col>)
+                    }
+                </Row>
+            }
         </div>
     );
 };
